2b6828c7e6e57e6198110e9bab3dde0d6887a573
Jordi-R66 <66806189+Jordi-R66@users.noreply.github.com>
2025-05-18 17:36:24 +0200

    Ajout des fichiers music.h et music.c avec la définition de la structure Music

---
+++ includes/music.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include "common.h"
+#include "dates.h"
+#include "entities.h"
+
+typedef uint16 musicId;
+
+#pragma pack(1)
+typedef struct {
+	musicId id;
+	string title;
+	Date releaseDate;
+	Time duration;
+	Artist* artists[];
+} Music;
