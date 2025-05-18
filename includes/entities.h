d712919af23ce00a61002df33f2072e89ebaa6e6
Jordi-R66 <66806189+Jordi-R66@users.noreply.github.com>
2025-05-18 17:50:32 +0200

    Ajout de l'ID dans la structure Person et réorganisation de la structure Band pour inclure un tableau d'artistes

--- includes/entities.h
+++ includes/entities.h
@@ -3,10 +3,20 @@
 #include "common.h"
 #include "dates.h"
 
+
+
+#pragma pack(1)
 typedef struct {
+	Id id;
 	string name;
 	string firstname;
 	Date birth;
 } Person;
 
 typedef Person Artist;
+
+typedef struct {
+	Id id;
+	Artist** members;
+} Band;
+#pragma pack()
